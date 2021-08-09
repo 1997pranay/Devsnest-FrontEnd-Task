784180023b2b433e6c2bc99282d81cbb4cc4e51f
Pranay Garg <41771750+1997pranay@users.noreply.github.com>
2021-08-09 22:20:29 +0530

    Add files via upload

---
+++ THA-23/custom-hooks/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import useToggle from "./useToggle";
+function App() {
+  const [isToggled, setToggle] = useToggle(false);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <button onClick={setToggle}>
+          {isToggled ? "Hello" : "Click this button"}
+        </button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
